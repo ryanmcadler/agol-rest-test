ec1ab44de33714914db4d8b1a278c3f6c0692bfd
ryanmcadler <ryanmcadler@gmail.com>
2017-05-26 09:51:03 -0700

    get applyEdits call to work

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ var request = require('request');
 
 var tokenURL = "https://www.arcgis.com/sharing/rest/oauth2/token/";
 var tokenParams = {
-  client_id: 'B1yWH9wx7WwscH8J',
-  client_secret: '9aed305482a14ad08a7ea770d8dc32dc',
+  client_id: process.env.CLIENT_ID,
+  client_secret: process.env.CLIENT_SECRET,
   grant_type: 'client_credentials'
 };
 
 var applyEditsURL = 'https://services1.arcgis.com/CHRAD8xHGZXuIQsJ/arcgis/rest/services/Incident_Monitor_Pin_Test/FeatureServer/0/applyEdits'
-var edits = "[{'geometry':{'x':-122.61806,'y':38.43288},'attributes':{'NAME':'Node test fire 2'}}]";
+var edits = [
+  {
+    'geometry':{'x':-122.61806,'y': 39.43288},
+    'attributes':{
+      'NAME':'Node test fire 3'
+    }
+  }
+];
 
 request.post({ url: tokenURL, formData: tokenParams }, function(err, res, body) {
 
@@ -16,7 +23,7 @@ request.post({ url: tokenURL, formData: tokenParams }, function(err, res, body) 
     var response = JSON.parse(body);
     var params = {
       form: {
-        adds: edits,
+        adds: JSON.stringify(edits),
         token: response.access_token,
         f: 'json'
       }
